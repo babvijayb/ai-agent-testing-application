63ee607bea00f74c26f0b08fc2a6e0ff7141c640
babvijayb <168264056+babvijayb@users.noreply.github.com>
2025-09-06 16:41:20 +0530

    Sample Form Insertion

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Form from './components/Form'
 import Counter from './components/Button'
+import PriceToggle from './components/PriceToggle'
+import SampleForm from './components/SampleForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Form />
-      <Counter />
+      {/* <Form /> */}
+      <SampleForm />
+      {/* <Counter /> */}
+      {/* <PriceToggle /> */}
     </>
   )
 }
